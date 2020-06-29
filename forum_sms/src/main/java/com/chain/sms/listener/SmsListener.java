5f5e9b28153b17206ab71f4a93e68782bab3c2f7
chenqian091 <chenqian091@ke.com>
2020-06-29 17:41:19 +0800

    commit

---
+++ forum_sms/src/main/java/com/chain/sms/listener/SmsListener.java
@@ -0,0 +1,26 @@
+package com.chain.sms.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * 消息监听器
+ */
+@Slf4j
+@Component
+@RabbitListener(queues = "sms")
+public class SmsListener {
+
+    /**
+     * 处理消息
+     * @param map
+     */
+    @RabbitHandler
+    public void  executeSms(Map<String,String> map){
+        log.info("消息处理 mobile:{} , checkCode:{}",map.get("mobile"),map.get("code"));
+    }
+}
